48d47ea7700132dfd93fd79068eba6b3254ddc9f
MonsterDKing <168490457+PslasMyDiet@users.noreply.github.com>
2024-10-29 14:15:37 -0600

    documentation and sequelize

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const port = process.env.PORT || 3001;
+
+  
+  //prefix
+  const globalPrefix = 'api/v1';
+  app.setGlobalPrefix(globalPrefix);
+
+
+  //enable cors
+  app.enableCors();
+
+  //validation pipe
+  app.useGlobalPipes(new ValidationPipe());
+
+
+
+  const options = new DocumentBuilder()
+    .setTitle('Api Documentation')
+    .setDescription('Api Documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(port);
 }
 bootstrap();
